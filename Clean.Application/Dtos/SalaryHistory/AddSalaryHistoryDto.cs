07c0accfaf36fa45dab08291687e8bc53196d8af
Ezoza <ezoza@MacBook-Pro-Ezoza.local>
2025-10-26 20:17:09 -0700

    My updates

---
+++ Clean.Application/Dtos/SalaryHistory/AddSalaryHistoryDto.cs
@@ -0,0 +1,9 @@
+namespace Clean.Application.Dtos.SalaryHistory;
+
+public class AddSalaryHistoryDto
+{
+    public int EmployeeId { get; set; }
+    public DateOnly Month { get; set; }
+    public decimal BaseAmount { get; set; }
+    public decimal BonusAmount { get; set; }
+}
